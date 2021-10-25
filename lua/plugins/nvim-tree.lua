9800e2237917e73c6ebd5122cdd5827785ac1c92
Adnios <2780199647@qq.com>
2021-10-25 18:32:18 +0800

    feat(theme): nvim-tree use dracula theme

--- lua/plugins/nvim-tree.lua
+++ lua/plugins/nvim-tree.lua
@@ -7,6 +7,56 @@ vim.g.nvim_tree_ignore = { '.DS_Store', 'fugitive:', '.git' }
 vim.g.nvim_tree_highlight_opened_files = 1
 vim.g.nvim_tree_root_folder_modifier = table.concat { ":t:gs?$?/..", string.rep(" ", 1000), "?:gs?^??" }
 
+local colors = {
+	bg = "#282A36",
+	fg = "#F8F8F2",
+	selection = "#44475A",
+	comment = "#6272A4",
+	red = "#FF5555",
+	orange = "#FFB86C",
+	yellow = "#F1FA8C",
+	green = "#50fa7b",
+	purple = "#BD93F9",
+	cyan = "#8BE9FD",
+	pink = "#FF79C6",
+	bright_red = "#FF6E6E",
+	bright_green = "#69FF94",
+	bright_yellow = "#FFFFA5",
+	bright_blue = "#D6ACFF",
+	bright_magenta = "#FF92DF",
+	bright_cyan = "#A4FFFF",
+	bright_white = "#FFFFFF",
+	menu = "#21222C",
+	visual = "#3E4452",
+	gutter_fg = "#4B5263",
+	nontext = "#3B4048",
+	white = "#ABB2BF",
+	black = "#191A21"
+}
+
+local highlight = function(group, fg, bg, attr, sp)
+    fg = fg and "guifg=" .. fg or "guifg=NONE"
+    bg = bg and "guibg=" .. bg or "guibg=NONE"
+    attr = attr and "gui=" ..attr or "gui=NONE"
+	sp = sp and "guisp=" .. sp or ""
+
+    vim.api.nvim_command("highlight " .. group .. " ".. fg .. " " .. bg .. " ".. attr .. " " .. sp)
+end
+
+
+highlight("NvimTreeRootFolder", colors.purple, nil, 'bold', nil)
+highlight("NvimTreeNormal ", colors.bright_blue, nil, nil, nil)
+highlight("NvimTreeGitDirty", colors.yellow, nil, nil, nil)
+highlight("NvimTreeGitNew", colors.bright_green, nil, nil, nil)
+highlight("NvimTreeImageFile", colors.pink, nil, nil, nil)
+highlight("NvimTreeFolderIcon", colors.purple, nil, nil, nil)
+highlight("NvimTreeIndentMarker", colors.nontext, nil, nil, nil)
+highlight("NvimTreeEmptyFolderName", colors.comment, nil, nil, nil)
+highlight("NvimTreeFolderName", colors.bright_blue, nil, nil, nil)
+highlight("NvimTreeSpecialFile", colors.pink, nil, 'underline', nil)
+-- vim.cmd('highlight NvimTreeRootFolder guifg=#7987A0 gui=bold')
+-- vim.cmd('highlight NvimTreeRootFolder guifg=LightMagenta gui=bold,italic')
+-- vim.cmd('highlight NvimTreeGitDirty guifg=#F1FA8C, gui=bold,italic')
 
 vim.g.nvim_tree_icons = {
    default = "",
@@ -61,7 +111,8 @@ require('nvim-tree').setup {
                 {
                     key = {"<CR>", "o", "<2-LeftMouse>"},
                     cb = tree_cb("edit")
-                }, {key = {"<2-RightMouse>", "<C-]>"}, cb = tree_cb("cd")},
+                },
+                {key = {"<2-RightMouse>", "<C-]>"}, cb = tree_cb("cd")},
                 {key = "<C-v>", cb = tree_cb("vsplit")},
                 {key = "<C-x>", cb = tree_cb("split")},
                 {key = "<C-t>", cb = tree_cb("tabnew")},
