f74563a02bc4d8653a405815c9126905d52191de
Adnios <2780199647@qq.com>
2021-10-26 13:01:08 +0800

    feat(comment): change to numToStr/Comment.nvim

--- lua/plugins/neogit.lua
+++ lua/plugins/neogit.lua
@@ -22,16 +22,6 @@ neogit.setup {
   integrations = {
     -- Neogit only provides inline diffs. If you want a more traditional way to look at diffs, you can use `sindrets/diffview.nvim`.
     -- The diffview integration enables the diff popup, which is a wrapper around `sindrets/diffview.nvim`.
-    --
-    -- Requires you to have `sindrets/diffview.nvim` installed.
-    -- use { 
-    --   'TimUntersberger/neogit', 
-    --   requires = { 
-    --     'nvim-lua/plenary.nvim',
-    --     'sindrets/diffview.nvim' 
-    --   }
-    -- }
-    --
     diffview = true
   },
   -- override/add mappings
@@ -49,6 +39,3 @@ neogit.setup {
 neogit.config.use_magit_keybindings()
 
 vim.api.nvim_set_keymap('n', '<leader>gn', '<cmd>Neogit<CR>', {noremap=false, silent=true})
--- vim.api.nvim_set_keymap('n', '<leader>gc', '<cmd>Neogit commit<CR>', {noremap=false, silent=true})
--- vim.api.nvim_set_keymap('n', '<leader>go', 'Neogit kind="split"', {noremap=false, silent=true})
--- vim.api.nvim_set_keymap('n', '<leader>gc', 'Neogit commit', {noremap=false, silent=true})
