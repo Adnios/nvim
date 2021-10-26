d656265d908aad63a40ca490c86e0c8ef05fb7f7
Adnios <2780199647@qq.com>
2021-10-26 18:20:11 +0800

    refactor(ui): remove useless offsets in bufferline and change the highlight

--- lua/plugins/bufferline.lua
+++ lua/plugins/bufferline.lua
@@ -11,33 +11,18 @@ require('bufferline').setup {
     -- custom_filter = custom_filter,
     offsets = {
       {
-        filetype = 'undotree',
-        text = 'Undotree',
-        highlight = 'PanelHeading',
-        padding = 1,
+        filetype = 'NvimTree',
+        text = 'Explorer',
+        -- highlight = 'PanelHeading',
+        highlight = 'Directory',
+        -- padding = 1,
       },
-      -- {
-      --   filetype = 'NvimTree',
-      --   text = 'Explorer',
-      --   highlight = 'PanelHeading',
-      --   padding = 1,
-      -- },
       {
         filetype = 'DiffviewFiles',
         text = 'Diff View',
-        highlight = 'PanelHeading',
-        padding = 1,
-      },
-      {
-        filetype = 'flutterToolsOutline',
-        text = 'Flutter Outline',
-        highlight = 'PanelHeading',
-      },
-      {
-        filetype = 'packer',
-        text = 'Packer',
-        highlight = 'PanelHeading',
-        padding = 1,
+        -- highlight = 'PanelHeading',
+        highlight = 'Directory',
+        -- padding = 1,
       },
     },
     -- groups = {
