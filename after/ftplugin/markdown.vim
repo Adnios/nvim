41a1cf57a0b18fbcb504c313084c283c35a9d5df
Adnios <2780199647@qq.com>
2022-09-17 16:42:57 +0800

    update

--- after/ftplugin/markdown.vim
+++
@@ -1,4 +0,0 @@
-setlocal shiftwidth=4
-setlocal softtabstop=-1
-setlocal tabstop=4
-setlocal autoindent
--- lua/plugins/nvim-tree.lua
+++
@@ -1,5 +0,0 @@
-require('nvim-tree').setup({
-  disable_netrw = false,
-  hijack_cursor = true,
-  hijack_netrw = true,
-})
